4288c39e4facb4e9009a965dadb7e74114d12aa0
bhuvana04 <48976822+bhuvana04@users.noreply.github.com>
2019-12-06 11:28:25 +0530

    Update arithmetic operations.c

--- arithmetic operations.c
+++ arithmetic operations.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+int (*fptr[2])(int,int);
 int sum(int x,int y)
 {
 int z=x+y;
@@ -13,8 +14,10 @@ return z;
 int main()
 {
     int a=10,b=5,c,d;
-    c=sum(a,b);
-    d=diff(a,b);
+    fptr[0]=sum;
+    c=fptr[0](a,b);
+    fptr[1]=diff;
+    d=fptr[1](a,b);
     printf("%d\n%d\n",c,d);
     return 0;
 }
